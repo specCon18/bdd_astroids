f0d7ef7cce1664ded11ac60abd9616d98eef24f8
specCon18 <steven.carpenter@skdevstudios.com>
2025-01-16 11:37:57 -0500

    added astroids and astroid field

---
+++ asteroid.py
@@ -0,0 +1,16 @@
+import circleshape
+import pygame
+import constants
+
+class Asteroid(circleshape.CircleShape):
+    
+    containers = []
+
+    def __init__(self, x, y, radius):
+         super().__init__(x,y,radius)
+
+    def draw(self,screen):
+        pygame.draw.circle(screen,(255,255,255),self.position,self.radius,2)
+
+    def update(self, dt):
+        self.position += self.velocity * dt
--- main.py
+++ main.py
@@ -1,26 +1,46 @@
 import pygame
 from constants import *
 from player import Player
+from asteroidfield import AsteroidField
+from asteroid import Asteroid
 
 def main():
+
+    # Print basic starting info
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+   
+    # Initialize game, screen, game-clock and delta time counter
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
     
+    # Groups
     drawables = pygame.sprite.Group()
     updatables = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
+    # Set the containers of the astroids
+    Asteroid.containers = (asteroids,drawables,updatables)
+
+    # Set the containers of the astroid_field
+    AsteroidField.containers = (updatables)
+    af = AsteroidField()
+
+    # Set the containers of player
     Player.containers = (drawables, updatables)
+    # Set the position of the player
     p1 = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
     
     while True:
+        # if event is quit then kill the game
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        # fill the screen with the void of space
         screen.fill((0, 0, 0))
         
         for drawable in drawables:
